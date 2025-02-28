05a79375df4abbf5487dc004c1357128986e96c0
Judson Lester <nyarly@gmail.com>
2025-02-27 16:17:53 -0800

    feat: switch to flake

---
+++ flake.nix
@@ -0,0 +1,66 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-23.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, flake-utils, nixpkgs }: (flake-utils.lib.eachDefaultSystem (system: let
+    pkgs = nixpkgs.legacyPackages.${system};
+
+    rubyEnv = pkgs.bundlerEnv {
+      inherit (pkgs) ruby;
+
+      name = "jekyll-blog";
+
+      gemfile = ./Gemfile;
+      lockfile = ./Gemfile.lock;
+      gemset = ./gemset.nix;
+    };
+  in
+    {
+    packages.blog = pkgs.stdenv.mkDerivation {
+      name = "blog-jdl";
+
+      src = if builtins.pathExists(./source.json) then
+        builtins.fetchGit (
+          let
+            source = builtins.fromJSON (builtins.readFile ./source.json);
+          in
+            {
+            url = "git@github.com:nyarly/blog.git";
+            inherit (source) rev;
+          }
+        )
+      else
+        pkgs.nix-gitignore.gitignoreSource ["_drafts/"] ./.;
+
+      buildInputs = with pkgs; [
+        libmysqlclient
+        mysql
+        bundler
+        bundix
+        nix-prefetch-git
+        rubyEnv
+      ];
+
+      buildPhase = "jekyll build";
+      installPhase = "cp -a _site $out";
+    };
+
+    packages.default = self.packages.${system}.blog;
+
+    devShells.default = pkgs.mkShell {
+      buildInputs = with pkgs; [
+        libmysqlclient
+        mysql
+        bundler
+        bundix
+        nix-prefetch-git
+        rubyEnv
+      ];
+    };
+  }
+  ));
+}
